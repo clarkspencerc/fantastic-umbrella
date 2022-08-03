6ff426b6c207ee43dbf5677e6b00bde006d6884b
clarkspencerc <clarkspencerc@gmail.com>
2022-08-03 10:01:32 -0700

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,18 +16,42 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [{ model: Product, through: ProductTag}],
+    where: { id: req.params.id }
+  }).then(data => {
+    res.json(data)
+  }).catch(err => res.status(400).json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then(data => {
+      res.json(data)
+    }).catch(err => res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      res.json(data)
+    }).catch(err => res.status(400).json(err))
 });
-
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    res.json(data)
+  }).catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
